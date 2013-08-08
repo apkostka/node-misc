4a277ed2975c00e84a65faf09ab517115b4769ff
Andrew Kostka <apkostka@gmail.com>
2013-08-08 08:28:56 -0700

    sockets

--- app.js
+++ app.js
@@ -1,32 +1,35 @@
-var express = require('express'),
-		jade = require('jade'),
-		models = require('./app/models.js'),
-		newUser,
-		app = express();
-
-app.configure('development', function() {
-	app.use(express.logger());
-  app.use(express.errorHandler({
-      dumpExceptions: true,
-      showStack: true
-  }));
-});
+var jade = require('jade'),
+	Twit = require('twit'),
+	configure = require('./app/config.js'),
+	models = require('./app/models.js'),
+	newUser,
+	app = require('express')(),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server);
 
 app.set('views', __dirname + '/app/views')
 app.set('view engine', 'jade')
-app.use(express.static(__dirname + '/assets'))
-app.use(express.bodyParser())
+app.use(require('express').bodyParser())
+
+server.listen(8080)
 
 // GET / - Get Users
 app.get('/', function(req, res) {
 	models.User.find(function(err, users){
 		res.render('index',
 			{ 
-				title: 'Home',
-				users: users
+				title: 'Home'
+				//users: users
 			}
 		)
 	})
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
 
 // POST / - Create User
@@ -37,14 +40,13 @@ app.post('/', function(req, res) {
 	res.redirect('/')
 })
 
-// GET /*USER*/DELETE - Delete user
+// GET /:USER/DELETE - Delete user
 app.get('/:id/delete', function(req, res) {
 	models.User.findByIdAndRemove(req.params.id, function(err, user) {
 		if (err) res.send(500, { error: err })
 		res.redirect('/')
 	})
 })
-
 
 //Presto.
 app.listen(3000);
