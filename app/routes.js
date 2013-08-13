ece9558d72095d2f16ee6129a09ec6e7238c41c7
Andrew Kostka <akostka@gunnjerkens.com>
2013-08-13 15:26:47 -0700

    service testing

--- app/routes.js
+++ app/routes.js
@@ -2,19 +2,23 @@ module.exports = function(app, mongoose, models, helpers){
 
 	//GET /
 	app.get('/', function(req, res){
+		
 		/*Twitter
 		helpers.twitter.get('/search/tweets', { q: 'NHL', count: 1 }, function(err, reply){
 			if(err) { res.send(err) }
 			res.send(reply.statuses)
 		})
 		*/
+
+		/*Facebook
 		helpers.facebook.api('brookfieldla/posts', {access_token: '580626551988812|1ukVg4Yr_gtyzFAt2vrZUmyRpoQ' }, function(reply){
 			if(!res || res.error) {
 				console.log(res.error)
 				return;
 			}
-			res.send(reply.data);
+			res.send(reply)
 		})
+		*/
 		
 	})
 
